aec12ee9ad9ddb18ad1f8abb9df30108db0e2b4c
mateusregasi <mateusregasi123@gmail.com>
2025-07-16 15:37:39 -0300

    Quase terminando minha parte

--- src/components/Artigos.tsx
+++ src/components/Artigos.tsx
@@ -1,9 +1,24 @@
-import "./css/Artigos.css"
+import './css/CardArtigo.css'
+import './css/Artigos.css'
+import CardArtigo from "./CardArtigo.tsx"
 
 const Artigos = () => {
-  return (
-    <div>Artigos</div>
-  )
+  const lista: number[] = [1,2,3,4,5,6]
+  
+  return (<>
+    <h1 className="title">Artigos</h1>
+    <p className='subtitle'>Clique para ler!</p>
+
+    <div className="container-sm d-lg-flex align-items-between flex-wrap justify-content-between">
+
+      {lista.map((v, i) => 
+        <div className="card">
+          <CardArtigo key={i} title="Title" pre="Kosdjaiosahduiashdiosaj"/>
+        </div>
+      )} 
+    </div>
+       
+  </>)
 }
 
 export default Artigos
