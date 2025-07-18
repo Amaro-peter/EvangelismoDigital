5b780601423955f46d4ec418be8d608631dc87a1
Danilo Alexandre <daniloabs@id.uff.br>
2025-07-18 15:09:24 -0300

    Formulário em estágios iniciais de desenvolvimento.

--- src/components/FormContato.tsx
+++ src/components/FormContato.tsx
@@ -3,11 +3,33 @@ import "./css/FormContato.css"
 
 function FormContato(){
     return (<>
-        <div className="formcontato">
+        <div className="modal-body">
             <form action="" name="form">
-                <label>Nome: <input type="text"/></label>
-                <label>Email: <input type="text"/></label>
-                <button type="submit">Enviar</button>
+                <fieldset className="form-group">
+                    {/* Formulário de contato */}
+                    <div className="form-group row">
+                        <label htmlFor="nome" className="col-form-label col-lg-2">Nome: </label>
+                        <br/>
+                        <div className="col-lg-10">
+                            <input type="text" id="nome" name="nome" className="form-control"/>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label htmlFor="email" className="col-form-label col-lg-2">Email: </label>
+                        <br/>
+                        <div className="col-lg-10">
+                            <input type="text" id="email" name="email" className="form-control"/>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <div className="col-lg-6 offset-lg-2">
+                            <button type="button" className="btn btn-primary">Enviar</button>
+                        </div>
+                    </div>
+
+                </fieldset>
             </form>
         </div>
 
