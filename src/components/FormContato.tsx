4f66168bc7f05a8ae93bf0ad1e651cca5705c0c3
Guilherme Pimentel <145625700+guipimentel30@users.noreply.github.com>
2025-10-22 17:17:17 -0300

    Add react-hook-form and zod validation to FormContato
    Integrated react-hook-form and zod for form validation in FormContato.tsx, providing schema-based validation and error messages for name and email fields. Updated related CSS for error message styling and adjusted form layout. Updated dependencies in package.json to include @hookform/resolvers, react-hook-form, and zod.

--- src/components/FormContato.tsx
+++ src/components/FormContato.tsx
@@ -1,14 +1,34 @@
 
 import "./css/FormContato.css"
+import { useForm } from "react-hook-form";
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const formContatoSchema = z.object({
+  name: z.string().min(1, "Digite um nome válido."),
+  email: z.string().email("Digite um email válido."),
+  acceptJesus: z.boolean().optional(),
+});
 
 function FormContato(){
+type FormContatoData = z.infer<typeof formContatoSchema>;
+
+    const { register, handleSubmit, formState:{ errors }, } = useForm<FormContatoData>({
+    resolver: zodResolver(formContatoSchema),
+    });
+
+    function handleFormContato(data: FormContatoData){
+    }
+
     return (<>
         <div className="formcontato">
-            <form action="" name="form">
-                <label>Nome: <input type="text"/></label>
-                <label>Email: <input type="text"/></label>
+            <form onSubmit={handleSubmit(handleFormContato)} name="form">
+                <label>Nome: <input type="text" {...register('name') }/></label>
+                {<p className="error-message">{errors.name ? errors.name.message : "\u00A0"}</p>}
+                <label>Email: <input type="email" {...register('email')} /></label>
+                {<p className="error-message">{errors.email ? errors.email.message : "\u00A0"}</p>}
                 <label className="checkbox-label">
-                    <input type="checkbox" className="checkbox-input"/>
+                    <input type="checkbox" className="checkbox-input" {...register('acceptJesus')}/>
                     Tomou uma decisão por Jesus hoje? Clique aqui.
                 </label>
                 <button type="submit">Enviar</button>
@@ -19,3 +39,5 @@ function FormContato(){
 }
 
 export default FormContato
+
+
