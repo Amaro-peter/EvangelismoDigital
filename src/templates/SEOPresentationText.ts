d6432ae1b0a8ea296cec749e9732143651a49726
Amaro-peter <ph_amaro@id.uff.br>
2025-12-21 23:12:41 -0300

    fix: updating SEO text for social media

--- src/templates/SEOPresentationText.ts
+++ src/templates/SEOPresentationText.ts
@@ -1,3 +1,3 @@
 export const seoPresentationText = `
-    Um hub de conteúdo que une razão e fé para responder às grandes questões da vida
+    Somos um projeto de evangelismo digital que busca responder perguntas sobre ciência, fé e questões existenciais.  Explore mais artigos e conheça nossas redes sociais para continuar essa jornada conosco. 
 `
