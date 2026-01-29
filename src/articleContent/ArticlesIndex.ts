ea4c1987c87fa7ad806ab546ec4413586732bad7
mateusregasi <mateusregasi123@gmail.com>
2026-01-29 20:06:30 -0300

    FEAT: paginação

--- src/articleContent/ArticlesIndex.ts
+++ src/articleContent/ArticlesIndex.ts
@@ -16,5 +16,60 @@ export const articlesIndex = [
     title: "Se Deus é bom, por que existe o mal?",
     imgpath: "o-mal.webp",
     pre: "Uma resposta para a dor e o sofrimento no mundo"
-  }
+  },
+  {
+    id: 'deusExiste',
+    title: "Deus existe?",
+    imgpath: "deus-existe.webp",
+    pre: "A razão pode nos levar a Deus?"
+  },
+  {
+    id: "caminhos",
+    title: "Todos os Caminhos levam a Deus?",
+    imgpath: "caminhos.webp",
+    pre: "Nem todos os caminhos levam aonde você pensa..."
+  },
+  {
+    id: 'oMal',
+    title: "Se Deus é bom, por que existe o mal?",
+    imgpath: "o-mal.webp",
+    pre: "Uma resposta para a dor e o sofrimento no mundo"
+  },
+  {
+    id: 'deusExiste',
+    title: "Deus existe?",
+    imgpath: "deus-existe.webp",
+    pre: "A razão pode nos levar a Deus?"
+  },
+  {
+    id: "caminhos",
+    title: "Todos os Caminhos levam a Deus?",
+    imgpath: "caminhos.webp",
+    pre: "Nem todos os caminhos levam aonde você pensa..."
+  },
+  {
+    id: 'oMal',
+    title: "Se Deus é bom, por que existe o mal?",
+    imgpath: "o-mal.webp",
+    pre: "Uma resposta para a dor e o sofrimento no mundo"
+  },
+  {
+    id: 'deusExiste',
+    title: "Deus existe?",
+    imgpath: "deus-existe.webp",
+    pre: "A razão pode nos levar a Deus?"
+  },
+  {
+    id: "caminhos",
+    title: "Todos os Caminhos levam a Deus?",
+    imgpath: "caminhos.webp",
+    pre: "Nem todos os caminhos levam aonde você pensa..."
+  },
+  {
+    id: 'oMal',
+    title: "Se Deus é bom, por que existe o mal?",
+    imgpath: "o-mal.webp",
+    pre: "Uma resposta para a dor e o sofrimento no mundo"
+  },
+
 ];
